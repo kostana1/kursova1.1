4c7779b7b76153df28100296e921214f594ac34f
kostana1 <62347337+kostana1@users.noreply.github.com>
2020-03-30 16:09:37 +0300

    bug found and code clean up

--- src/main/java/com/service/CommonUtils.java
+++ src/main/java/com/service/CommonUtils.java
@@ -1,9 +1,5 @@
 package com.service;
 
-import com.enumex.EGender;
-import com.enumex.EStatus;
-import com.person.Person;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,14 +10,12 @@ public class CommonUtils {
     public static final String USE_INTEGERS_ONLY = "Use integers as per description";
 
     private static final CreatePersonService createPersonService = new CreatePersonService();
-    private static Person person = new Person("", EGender.MALE, new Date(), "sdf", EStatus.IN_RELATIONSHIP);
 
     public static Date formatDateOfBirth(String dateOfBirthString) {
         Date dateOfBirth = null;
         if(createPersonService.isValidDateOfBirth(dateOfBirthString)) {
             try {
                 dateOfBirth = new SimpleDateFormat(PATTERN_DATE_OF_BIRTH).parse(dateOfBirthString);
-                person.setDateOfBirth(dateOfBirth);
             } catch (ParseException e) {
                 e.printStackTrace();
                 System.out.println(USE_INTEGERS_ONLY);
